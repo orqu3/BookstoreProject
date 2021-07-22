6367ec99faf8187a430893dc8ffca0105b93ced4
Mariia <marisergiv@gmail.com>
2021-07-22 18:02:47 +0300

    Added Role Entity, Role Repository and unit Tests.

---
+++ BookstoreCommon/src/main/java/com/bookstore/common/entity/Role.java
@@ -0,0 +1,56 @@
+package com.bookstore.common.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name", length = 40, nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description", length = 150, nullable = false)
+    private String description;
+
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Role(Integer id) {
+        this.id = id;
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id.equals(role.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
