eb7070b2c9f6920860d28c8e14c796825557b4c8
Mariia <marisergiv@gmail.com>
2021-07-22 14:54:01 +0300

    Initial commit

---
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/BookstoreAdminApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreAdminApplication.class, args);
+    }
+
+}
---
+++ BookstoreWebParent/BookstoreShopping/src/main/java/com/bookstore/shopping/BookstoreShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.bookstore.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreShoppingApplication.class, args);
+    }
+
+}
