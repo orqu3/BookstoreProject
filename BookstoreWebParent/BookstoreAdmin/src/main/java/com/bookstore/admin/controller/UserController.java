7f7e6b70d65d00d02d0c5b451f4e240b11889d2e
Mariia <marisergiv@gmail.com>
2021-07-24 01:09:54 +0300

    Implemented New User Function.

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/controller/UserController.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.bookstore.admin.controller;
 
 import com.bookstore.admin.service.UserService;
+import com.bookstore.common.entity.Role;
 import com.bookstore.common.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -21,4 +24,22 @@ public class UserController {
         model.addAttribute("users", users);
         return "users";
     }
+
+    @GetMapping("/users/new")
+    public String newUser(Model model) {
+        List<Role> roles = userService.listRoles();
+        User user = new User();
+        user.setEnabled(true);
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+        return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user, RedirectAttributes redirectAttributes) {
+        userService.save(user);
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+        return "redirect:/users";
+    }
+
 }
--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/service/UserService.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/service/UserService.java
@@ -1,6 +1,8 @@
 package com.bookstore.admin.service;
 
+import com.bookstore.admin.repository.RoleRepository;
 import com.bookstore.admin.repository.UserRepository;
+import com.bookstore.common.entity.Role;
 import com.bookstore.common.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,8 +14,17 @@ import java.util.List;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     public List<User> listAll() {
         return (List<User>) userRepository.findAll();
+    }
+
+    public List<Role> listRoles() {
+        return (List<Role>) roleRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
     }
 }
