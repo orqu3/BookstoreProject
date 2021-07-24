45c94ac536744b01a53174492ad515ee0e87377f
Mariia <marisergiv@gmail.com>
2021-07-24 23:58:17 +0300

    Implemented Checking Uniqueness of User Email.

---
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/controller/UserRestController.java
@@ -0,0 +1,19 @@
+package com.bookstore.admin.controller;
+
+import com.bookstore.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserRestController {
+
+    private final UserService userService;
+
+    @PostMapping("/users/check_email")
+    public String checkDuplicateEmail(@Param("email") String email) {
+        return  userService.isEmailUnique(email) ? "OK" : "Duplicated";
+    }
+}
