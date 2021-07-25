090db61f035aaa43173cdce6cb6dea7fc8b2db61
Mariia <marisergiv@gmail.com>
2021-07-25 23:46:38 +0300

    Implemented Updating User Enabled Status.

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/service/UserService.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/service/UserService.java
@@ -8,12 +8,14 @@ import com.bookstore.common.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UserService {
 
     private final UserRepository userRepository;
@@ -76,5 +78,9 @@ public class UserService {
             throw new UserNotFoundException("Could not find any user with ID " + id);
         }
         userRepository.deleteById(id);
+    }
+
+    public void updateUserEnabledStatus(Integer id, Boolean enabled) {
+        userRepository.updateEnabledStatus(id, enabled);
     }
 }
