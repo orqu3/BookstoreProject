f8168fbaed8d53440b970dada34a391f379841da
Mariia <marisergiv@gmail.com>
2021-07-29 00:47:29 +0300

    Added User Authorization Basics.

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfiguration.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfiguration.java
@@ -41,6 +41,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/users/**").hasAuthority("Admin")
                 .anyRequest()
                 .authenticated()
                 .and()
