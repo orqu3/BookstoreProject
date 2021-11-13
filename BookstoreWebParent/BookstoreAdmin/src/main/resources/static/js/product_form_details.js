b53a8e1f1f1f3833489f900edbb69827bee50552
Mariia <marisergiv@gmail.com>
2021-11-14 00:49:20 +0200

    Added Details on Product Form.

---
+++ BookstoreWebParent/BookstoreAdmin/src/main/resources/static/js/product_form_details.js
@@ -0,0 +1,33 @@
+function addNextDetailSection() {
+
+    allDivDetails = $("[id^='divDetail']");
+    divDetailsCount = allDivDetails.length;
+
+    htmlDetailSection = `
+        <div class="form-inline" id="divDetail${divDetailsCount}">
+        <label class="m-3">Name:</label>
+        <input type="text" class="form-control w-25" name="detailNames" maxlength="255"/>
+        <label class="m-3">Value:</label>
+        <input type="text" class="form-control w-25" name="detailValues" maxlength="255"/>
+    </div>
+    `;
+
+    $("#divProductDetails").append(htmlDetailSection);
+
+    previousDivDetailSection = allDivDetails.last();
+    previousDivDetailId = previousDivDetailSection.attr("id");
+
+    htmlLinkRemove = `
+        <a class="btn fas fa-times-circle fa-2x icon-silver"
+        href="javascript:removeDetailSectionById('${previousDivDetailId}')"
+        title="Remove this detail"></a>
+    `;
+
+    previousDivDetailSection.append(htmlLinkRemove);
+
+    $("input[name='detailNames']").last().focus();
+}
+
+function removeDetailSectionById(id) {
+    $("#" + id).remove();
+}
