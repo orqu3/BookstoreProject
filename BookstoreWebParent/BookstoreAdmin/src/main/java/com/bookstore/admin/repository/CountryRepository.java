15440b84989e5b5891c8e797516a9cab9858bff2
Andre <ncl8@mail.ru>
2021-11-19 03:46:37 +0300

    Code Controller Layer for Country

---
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.bookstore.admin.repository;
+
+import com.bookstore.common.entity.Country;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CountryRepository extends CrudRepository<Country, Integer> {
+    public List<Country> findAllByOrderByNameAsc();
+}
