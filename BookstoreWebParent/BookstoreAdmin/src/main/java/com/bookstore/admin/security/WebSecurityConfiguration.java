8b3fa71d2e664cb2b1100bce7161334543bb2fa6
Andre <ncl8@mail.ru>
2021-11-20 21:42:14 +0300

    Code Manage Countries - part 1

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfiguration.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfiguration.java
@@ -42,7 +42,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-            .antMatchers("/users/**", "/settings/**").hasAuthority("Admin")
+            .antMatchers("/users/**").hasAuthority("Admin")
             .antMatchers("/categories/**", "/products/**").hasAnyAuthority("Admin", "Salesperson")
             .anyRequest()
             .authenticated()
@@ -54,7 +54,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
             .logout()
             .permitAll();
-        http.csrf().disable();
     }
 
     @Override
