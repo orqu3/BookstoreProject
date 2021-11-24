225d330b93158ffbaf44361c7d3c22d1a085a8c7
Andre <ncl8@mail.ru>
2021-11-24 13:37:17 +0300

    Code Customer Management Module - part 1

---
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/controller/CustomerRestController.java
@@ -0,0 +1,22 @@
+package com.bookstore.admin.controller;
+
+import com.bookstore.admin.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CustomerRestController {
+    private final CustomerService service;
+
+    @PostMapping("/customers/check_email")
+    public String checkDuplicateEmail(@Param("id") Integer id, @Param("email") String email){
+        if(service.isEmailUnique(id, email)){
+            return "OK";
+        } else {
+            return "Duplicated";
+        }
+    }
+}
