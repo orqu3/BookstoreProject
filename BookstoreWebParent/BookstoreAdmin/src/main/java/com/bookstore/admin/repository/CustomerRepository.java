5c2562e6ac95e1eefc90d636da001405676988bc
Andre <ncl8@mail.ru>
2021-11-28 19:09:16 +0300

    Code Customer Management Module - part 4

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/repository/CustomerRepository.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/repository/CustomerRepository.java
@@ -6,23 +6,25 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
     @Query("SELECT c FROM Customer c WHERE concat(c.email, ' ', c.firstName, ' ', c.lastName, ' ', "
     +"c.addressLine1, ' ', c.addressLine2, ' ', c.city, ' ', c.state, "
     + "' ', c.postalCode, ' ', c.country.name) LIKE %?1%")
-    Page<Customer> findAll(String keyword, Pageable pageable);
+    public Page<Customer> findAll(String keyword, Pageable pageable);
 
     @Query("UPDATE Customer c SET c.enabled = ?2 WHERE c.id = ?1")
     @Modifying
-    void updateEnabledStatus(Integer id, boolean enabled);
+    public void updateEnabledStatus(Integer id, boolean enabled);
 
     @Query("SELECT c FROM Customer c WHERE c.email = ?1")
-    Customer findByEmail(String email);
+    public Customer findByEmail(String email);
 
-    Long countById(Integer id);
+    public Long countById(Integer id);
 
     @Query("SELECT c FROM Customer c WHERE c.verificationCode = ?1")
-    Customer findByVerificationCode(String code);
+    public Customer findByVerificationCode(String code);
 }
