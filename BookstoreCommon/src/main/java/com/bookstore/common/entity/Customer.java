f18f0c4d921c25fd2430ac12017f1ef567be2473
Andre <ncl8@mail.ru>
2021-11-28 20:09:16 +0300

    Small changes

--- BookstoreCommon/src/main/java/com/bookstore/common/entity/Customer.java
+++ BookstoreCommon/src/main/java/com/bookstore/common/entity/Customer.java
@@ -1,14 +1,17 @@
 package com.bookstore.common.entity;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Table(name = "customers")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class Customer {
 
