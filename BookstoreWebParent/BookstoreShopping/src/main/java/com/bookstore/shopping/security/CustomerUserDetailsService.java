7559cd65eb52de83220f573f0d85b20dec33b88a
Mariia <marisergiv@gmail.com>
2021-11-29 18:05:05 +0200

    Customer Login and Logout part 1.

---
+++ BookstoreWebParent/BookstoreShopping/src/main/java/com/bookstore/shopping/security/CustomerUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.bookstore.shopping.security;
+
+import com.bookstore.common.entity.Customer;
+import com.bookstore.shopping.repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Customer customer = customerRepository.findByEmail(email);
+        if (customer == null) {
+            throw new UsernameNotFoundException("No customer found with the email " + email);
+        }
+        return new CustomerUserDetails(customer);
+    }
+}
