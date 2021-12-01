0b35a38f06da7af42e1b9b161638d4c1565451a0
Mariia <marisergiv@gmail.com>
2021-12-01 11:13:46 +0300

    Update Authentication Type of Customer part 2.

--- BookstoreWebParent/BookstoreShopping/src/main/java/com/bookstore/shopping/service/CustomerService.java
+++ BookstoreWebParent/BookstoreShopping/src/main/java/com/bookstore/shopping/service/CustomerService.java
@@ -1,33 +1,36 @@
 package com.bookstore.shopping.service;
 
+import com.bookstore.common.entity.AuthenticationType;
 import com.bookstore.common.entity.Country;
 import com.bookstore.common.entity.Customer;
 import com.bookstore.shopping.repository.CountryRepository;
 import com.bookstore.shopping.repository.CustomerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerService {
 
-    private final CountryRepository countryRepo;
+    private final CountryRepository countryRepository;
+    private final CustomerRepository customerRepository;
 
-    private final CustomerRepository customerRepo;
-
-    public CustomerService(CountryRepository countryRepo, CustomerRepository customerRepo) {
-        this.countryRepo = countryRepo;
-        this.customerRepo = customerRepo;
-    }
 
     public List<Country> listAllCountries() {
-        return countryRepo.findAllByOrderByNameAsc();
+        return countryRepository.findAllByOrderByNameAsc();
     }
 
     public boolean isEmailUnique (String email) {
-        Customer customer = customerRepo.findByEmail(email);
+        Customer customer = customerRepository.findByEmail(email);
         return customer == null;
+    }
+
+    public void updateAuthentication(Customer customer, AuthenticationType type) {
+        if(!customer.getAuthenticationType().equals(type)) {
+            customerRepository.updateAuthenticationType(customer.getId(), type);
+        }
     }
 
 }
