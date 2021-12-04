ff19991987290c7b94356e6094fa586ae8529739
Andre <ncl8@mail.ru>
2021-12-05 00:50:09 +0300

    Code Customer Management Module - part 11.2

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/service/ProductService.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/service/ProductService.java
@@ -28,9 +28,10 @@ public class ProductService {
         return (List<Product>) productRepository.findAll();
     }
 
-    public Page<Product> listByPage(int pageNum, PagingAndSortingHelper helper, Integer categoryId) {
-        Pageable pageable = helper.createPageable(PRODUCTS_PER_PAGE, pageNum);
-        String keyword = helper
+    public Page<Product> listByPage(int pageNum, String sortField, String sortDir, String keyword, Integer categoryId) {
+        Sort sort = Sort.by(sortField);
+        sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+        Pageable pageable = PageRequest.of(pageNum - 1, PRODUCTS_PER_PAGE, sort);
 
         if (keyword != null && !keyword.isEmpty()) {
             if (categoryId != null && categoryId > 0) {
