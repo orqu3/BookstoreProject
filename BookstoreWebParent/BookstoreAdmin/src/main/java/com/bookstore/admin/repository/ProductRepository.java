f3344e158017ed2b9e850fc63f3329656f76575f
Mariia <marisergiv@gmail.com>
2021-12-07 17:43:53 +0300

    Revert "Code Customer Management Module - part 13"
    This reverts commit 4e91b118c41d7f287b72dc247d7a75c14815a1ea.

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/repository/ProductRepository.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package com.bookstore.admin.repository;
 
+import com.bookstore.admin.pagin.SearchRepository;
+import com.bookstore.common.entity.Customer;
 import com.bookstore.common.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
