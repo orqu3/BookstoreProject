b55ddf5caf1dedc1289b6c20aa2db905ec87eb49
Mariia <marisergiv@gmail.com>
2021-12-07 17:44:17 +0300

    Revert "Code Customer Management Module - part 11.2"
    This reverts commit ff19991987290c7b94356e6094fa586ae8529739.

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/service/ProductService.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/service/ProductService.java
@@ -28,10 +28,9 @@ public class ProductService {
         return (List<Product>) productRepository.findAll();
     }
 
-    public Page<Product> listByPage(int pageNum, String sortField, String sortDir, String keyword, Integer categoryId) {
-        Sort sort = Sort.by(sortField);
-        sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
-        Pageable pageable = PageRequest.of(pageNum - 1, PRODUCTS_PER_PAGE, sort);
+    public Page<Product> listByPage(int pageNum, PagingAndSortingHelper helper, Integer categoryId) {
+        Pageable pageable = helper.createPageable(PRODUCTS_PER_PAGE, pageNum);
+        String keyword = helper
 
         if (keyword != null && !keyword.isEmpty()) {
             if (categoryId != null && categoryId > 0) {
