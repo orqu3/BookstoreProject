f232e87729e222c349afb2c9d349540c5a4f5e89
Mariia <marisergiv@gmail.com>
2021-12-10 10:49:00 +0300

    Implemented Removing Product from Shopping Cart

--- BookstoreWebParent/BookstoreShopping/src/main/java/com/bookstore/shopping/controller/ShoppingCartRestController.java
+++ BookstoreWebParent/BookstoreShopping/src/main/java/com/bookstore/shopping/controller/ShoppingCartRestController.java
@@ -7,6 +7,7 @@ import com.bookstore.shopping.service.CustomerService;
 import com.bookstore.shopping.service.ShoppingCartService;
 import com.bookstore.shopping.util.Utility;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +56,18 @@ public class ShoppingCartRestController {
             return String.valueOf(subtotal);
         } catch (CustomerNotFoundException e) {
             return "You must login to change quantity of product.";
+        }
+    }
+
+    @DeleteMapping("/cart/remove/{productId}")
+    public String removeProduct(@PathVariable("productId") Integer productId, HttpServletRequest request) {
+        try {
+            Customer customer = getAuthenticatedCustomer(request);
+            cartService.removeProduct(productId, customer);
+
+            return "The product has been removed from your shopping cart.";
+        } catch (CustomerNotFoundException e) {
+            return "You must login to remove product.";
         }
     }
 }
