bf85c89210d1472bafdebe031ee733eb2f085dde
Andre <ncl8@mail.ru>
2021-12-10 18:58:49 +0300

    Code Orders Listing - part 1

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfiguration.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/security/WebSecurityConfiguration.java
@@ -41,7 +41,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/categories/**", "/products", "/products/", "/products/detail/**", "/products/page/**", "/products/edit/**", "/products/save", "/products/check_unique")
+                .antMatchers("/categories/**", "/products", "/products/", "/products/detail/**", "/products/page/**", "/products/edit/**", "/products/save", "/products/check_unique", "/customers/**", "/orders/**")
                     .hasAnyAuthority("Admin", "Salesperson")
                 .antMatchers("/users/**", "/products/**", "/settings/**", "/countries/**", "/states/**")
                     .hasAuthority("Admin")
