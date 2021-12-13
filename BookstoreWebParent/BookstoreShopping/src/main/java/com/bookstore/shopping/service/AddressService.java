675620ec798e4154ccc5a6cafb546fdcdc33acc7
Ruleton-at-301701292774 <catalex777@gmail.com>
2021-12-13 20:44:46 +0300

    Code Add, Edit and Delete Address

--- BookstoreWebParent/BookstoreShopping/src/main/java/com/bookstore/shopping/service/AddressService.java
+++ BookstoreWebParent/BookstoreShopping/src/main/java/com/bookstore/shopping/service/AddressService.java
@@ -6,11 +6,13 @@ import com.bookstore.shopping.repository.AddressRepository;
 import com.bookstore.shopping.util.Utility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Service
+@Transactional
 public class AddressService {
 
     private final AddressRepository repo;
@@ -23,6 +25,18 @@ public class AddressService {
 
     public List<Address> listAddressBook(Customer customer) {
         return repo.findByCustomer(customer);
+    }
+
+    public void save(Address address) {
+        repo.save(address);
+    }
+
+    public Address get(Integer addressId, Integer customerId) {
+        return repo.findByIdAndCustomer(addressId,customerId);
+    }
+
+    public void delete(Integer addressId, Integer customerId) {
+        repo.deleteByIdAndCustomer(addressId, customerId);
     }
 
 
