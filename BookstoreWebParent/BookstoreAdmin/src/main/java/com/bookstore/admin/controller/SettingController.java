84c40f38462405617719b413016c169c1010e815
Mariia <marisergiv@gmail.com>
2021-12-15 16:53:06 +0300

    Updated settings module

--- BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/controller/SettingController.java
+++ BookstoreWebParent/BookstoreAdmin/src/main/java/com/bookstore/admin/controller/SettingController.java
@@ -87,4 +87,14 @@ public class SettingController {
 
         return "redirect:/settings";
     }
+
+    @PostMapping("/settings/save_payment")
+    public String savePaymentSettings(HttpServletRequest request, RedirectAttributes ra) {
+        List<Setting> paymentSettings = settingService.getPaymentSettings();
+        updateSettingValuesFromForm(request, paymentSettings);
+
+        ra.addFlashAttribute("message", "Payment settings have been saved");
+
+        return "redirect:/settings";
+    }
 }
